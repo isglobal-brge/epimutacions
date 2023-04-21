9416edbd625c7c032c67d0aa36f8b8e4c6834d54
U-ISGLOBAL\dpelegri <dpelegri@ISG11207.isglobal.lan>
2023-04-21 04:43:45 +0200

    remove pca correction and version x.y.z bump

--- R/epi_pca_correction.R
+++ R/epi_pca_correction.R
@@ -1,43 +1,42 @@
-#' @title  PCA correction
-#' 
-#' @description  Compute PCA correction
-#' 
-#' @param cases a GenomicRatioSet object containing the case samples.
-#' See the constructor function \link[minfi]{GenomicRatioSet}, 
-#' \link[minfi]{makeGenomicRatioSetFromMatrix}. 
-#' @param controls a GenomicRatioSet object containing the 
-#' control panel (control panel).
-#' @param prange probe.range
-#' 
-#' @return A list with cases and controls corrected by PCs
-#' 
-#' 
-#' @examples 
-#' 
-#' data(res.epi.manova)
-#' #Annotate the epimutations
-#' 
-#' @importFrom stats residuals
-#' 
-PCA_correction <- function(cases, controls, prange = 40000) {
-    
-    ## Compute residuals of pcs
-    p <- intersect( featureNames(cases), featureNames(controls))
-    ## found 51 overlapping featureNames then combine the sets
-    all_samples <- Biobase::combine( cases[p, ], controls[p, ])
-    
-    beta <- meffil:::impute.matrix(minfi::getBeta(all_samples), margin = 1)
-    ndim <- isva::EstDimRMT(beta, FALSE)$dim + 1
-    pcs <- meffil::meffil.methylation.pcs(minfi::getBeta(all_samples), probe.range = prange)
-    m <- getM(all_samples)
-    res <- residuals(limma::lmFit(m, pcs[, seq_len(ndim)]), m)
-    beta <- ilogit2(res)
-    assay(all_samples) <- beta
-    
-    gr_cases <- all_samples[ , colnames(cases)]
-    gr_controls <- all_samples[ , colnames(controls)]
-    
-    return(list(cases = gr_cases, 
-                controls = gr_controls))
-    
-}
+#' #' @title  PCA correction
+#' #' 
+#' #' @description  Compute PCA correction
+#' #' 
+#' #' @param cases a GenomicRatioSet object containing the case samples.
+#' #' See the constructor function \link[minfi]{GenomicRatioSet}, 
+#' #' \link[minfi]{makeGenomicRatioSetFromMatrix}. 
+#' #' @param controls a GenomicRatioSet object containing the 
+#' #' control panel (control panel).
+#' #' @param prange probe.range
+#' #' 
+#' #' @return A list with cases and controls corrected by PCs
+#' #' 
+#' #' 
+#' #' @examples 
+#' #' 
+#' #' data(res.epi.manova)
+#' #' #Annotate the epimutations
+#' #' 
+#' #' 
+#' PCA_correction <- function(cases, controls, prange = 40000) {
+#'     
+#'     ## Compute residuals of pcs
+#'     p <- intersect( featureNames(cases), featureNames(controls))
+#'     ## found 51 overlapping featureNames then combine the sets
+#'     all_samples <- Biobase::combine( cases[p, ], controls[p, ])
+#'     
+#'     beta <- meffil:::impute.matrix(minfi::getBeta(all_samples), margin = 1)
+#'     ndim <- isva::EstDimRMT(beta, FALSE)$dim + 1
+#'     pcs <- meffil::meffil.methylation.pcs(minfi::getBeta(all_samples), probe.range = prange)
+#'     m <- getM(all_samples)
+#'     res <- residuals(limma::lmFit(m, pcs[, seq_len(ndim)]), m)
+#'     beta <- ilogit2(res)
+#'     assay(all_samples) <- beta
+#'     
+#'     gr_cases <- all_samples[ , colnames(cases)]
+#'     gr_controls <- all_samples[ , colnames(controls)]
+#'     
+#'     return(list(cases = gr_cases, 
+#'                 controls = gr_controls))
+#'     
+#' }
