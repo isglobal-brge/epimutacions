b9e17f3c450ed61f073b13a326c6c70634e108af
Carlos Ruiz Arenas <cruizarenas@unav.es>
2023-06-05 14:32:40 +0200

    Change computation for outliers

--- R/integrateReferenceQuantile.R
+++ R/integrateReferenceQuantile.R
@@ -16,7 +16,12 @@ normalizeBeta <- function(vals, beta_params){
   valsf <- vals[!outs]
   bpars <- getBetaParams(matrix(valsf, ncol = 1))
   
+  mean_ori <- beta_params[1] / (beta_params[1] + beta_params[2])
+  mean_new <- bpars[1] / (bpars[1] + bpars[2])
+  
   ps <- pbeta(vals, bpars[1], bpars[2])
   vals_pit <- qbeta(ps, beta_params[1], beta_params[2])
   
+  vals_pit[outs] <- vals[outs] + (mean_ori - mean_new)
+  vals_pit
 }
